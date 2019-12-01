077f89ae746f75030c77f1e2743c05e731f25c11
apaudyal <56798791+apaudyal@users.noreply.github.com>
2019-12-01 11:50:03 -0600

    My First Commit

---
+++ src/main/java/test/GoogleTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class GoogleTest {
+	@Test
+	
+	public void openGoogleSite () {
+	
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		
+
+}
+}
